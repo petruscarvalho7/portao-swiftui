8e2d5a4aee8dbe55bde1a7899acc411805ceb64b
petrus.carvalho <petrus.carvalho@logirix.com>
2023-08-11 10:53:11 +0100

    Initial Commit

---
+++ PortaoApp/PortaoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortaoAppApp.swift
+//  PortaoApp
+//
+//  Created by Petrus Carvalho on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PortaoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
